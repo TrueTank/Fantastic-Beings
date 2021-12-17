0987c413018d92b1fdd2763de2c5c6733df86fdb
TrueTank <mosta001@mail.ru>
2021-12-18 00:26:02 +0500

    add tests

--- Fantastic Beings/stage1/test/test.js
+++ Fantastic Beings/stage1/test/test.js
@@ -7,6 +7,7 @@ class FantasticBeingsTest extends StageTest {
     page = this.getPage(pagePath);
 
     tests = [
+        // Test#1 - check existence of board element
         this.page.execute(() => {
             this.board = document.getElementById('board');
 
@@ -14,6 +15,7 @@ class FantasticBeingsTest extends StageTest {
                 correct() :
                 wrong(`You need to create a game board with the ID "board"`)
         }),
+        // Test#2 - check existence of table element
         this.page.execute(() => {
             let boardChilds = this.board.children;
             for(let i of boardChilds) {
@@ -24,6 +26,7 @@ class FantasticBeingsTest extends StageTest {
 
             return wrong(`You need to create a table element for map in your board`)
         }),
+        // Test#3 - check existence of map element
         this.page.execute(() => {
             this.map = document.getElementById('map');
 
@@ -31,6 +34,7 @@ class FantasticBeingsTest extends StageTest {
                 correct() :
                 wrong(`You need to create a game board with the ID "board"`)
         }),
+        // Test#4 - check number of rows in table
         this.page.execute(() => {
             let rows = this.map.rows.length;
 
@@ -38,6 +42,7 @@ class FantasticBeingsTest extends StageTest {
                 correct() :
                 wrong(`You need to create a map with 10 rows, found ${rows}`)
         }),
+        // Test#5 - check number of cells in map
         this.page.execute(() => {
             let tds = document.getElementsByTagName('td').length;
             let cells = document.getElementsByClassName('cell').length;
@@ -46,21 +51,63 @@ class FantasticBeingsTest extends StageTest {
                 correct() :
                 wrong(`You need to create a map with 100 cells, found ${cells} cells and ${tds} 'td' elements`)
         }),
-        /*this.node.execute(async () => {
-            let board = await this.page.findById('board');
-            let boardStyles = await board.getComputedStyles();
-            return boardStyles ?
-                correct() :
-                wrong(`You need to set background image for your board`)
-        }),
-
+        // Test#6 - check borders of cells
         this.page.execute(() => {
-            let bodyRect = await document.body.getComputedStyles();
+            this.cells = document.getElementsByClassName('cell');
+            let i = 0;
+            for (let elem of this.cells) {
+                i++;
+                let currBorder = window.getComputedStyle(elem).border;
+                if (currBorder.includes('0px') || !currBorder.includes('rgb(147, 112, 219)')) {
+                    return wrong(`Looks like cell of game map #${i} ` +
+                        `has no border. It should have a mediumpurple border.`);
+                }
+            }
+            return correct()
+        }),
+        // Test#7 - check form of cells
+        this.page.execute(() => {
+            let i = 0;
+            for (let elem of this.cells) {
+                i++;
+                let width = window.getComputedStyle(elem).width;
+                let height = window.getComputedStyle(elem).height;
+                if (width !== height) {
+                    return wrong(`Looks like cell of game map #${i} ` +
+                        `has invalid sizes.`);
+                }
+            }
+            return correct()
+        }),
+        // Test#8 - check size of cells
+        this.page.execute(() => {
+            let i = 0;
+            let bodyHeight = window.getComputedStyle(document.body).height;
+            let requiredCellHeight = parseInt(0.06*bodyHeight.replace('px', ''));
+            for (let elem of this.cells) {
+                i++;
+                let height = window.getComputedStyle(elem).height;
+                if (parseInt(height.replace('px', '')) !== requiredCellHeight) {
+                    return wrong(`Looks like cell of game map #${i} ` +
+                        `has invalid sizes.`);
+                }
+            }
+            return correct()
+        }),
+        // Test#9 - check position of map
+        this.page.execute(() => {
+            let mapStyle = window.getComputedStyle(this.map);
+            let boardWidth = window.getComputedStyle(this.board).width.replace('px', '');
+            let mapWidth = mapStyle.width.replace('px', '');
+            let mapMarginLeft = mapStyle.marginLeft.replace('px', '');
 
-            return bodyRect.top === 0 && bodyRect.left === 0 && bodyRect.right === 0 ?
+            return (boardWidth - mapWidth - mapMarginLeft == mapMarginLeft) ?
                 correct() :
-                wrong(`You need to drop any margins or paddings for your game board`)
-        }),*/
+                wrong(`You need to position the game map in the center relative to the horizontal`)
+        }),
+        // Test#10 - check height of board
+        // Test#11 - check background images
+        // Test#12 - check size of status-bar
     ]
 
 }
